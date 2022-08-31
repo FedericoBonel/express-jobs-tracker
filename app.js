044a5b52615b8f2dce6e0c204e778c5c2b2eaf27
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-08-31 10:39:39 +0900

    Project initialization

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+
+const PORT = process.env.PORT;
+const API_BASE_URL = process.env.API_BASE_URL;
+
+const app = express();
+
+app.use(express.json());
+
+const startServer = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
+    } catch(error) {
+        console.log(`An error happened during server start: ${error}`);
+    }
+}
