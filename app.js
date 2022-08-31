01ef174f34cba99db89c9af62ee7b16f75a0b5cd
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-08-31 11:59:58 +0900

    Added logger using morgan npm

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const express = require("express");
 
 const jobsRoutes = require("./src/routes/JobsRoutes");
 const authRoutes = require("./src/routes/AuthRoutes");
+const logger = require("./src/middleware/Logger")
 const notFoundHandler = require("./src/middleware/NotFound");
 const errorHandler = require("./src/middleware/ErrorHandler");
 const connectToDB = require("./src/db/Connect");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const API_BASE_URL = process.env.API_BASE_URL;
 
 const app = express();
 
-app.use(express.json());
+app.use(logger, express.json());
 
 
 app.use(`${API_BASE_URL}/jobs`, jobsRoutes);
